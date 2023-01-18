99a3fcf1b664a960e9a91941513f3e801041b1c0
Muh Farkhan Syafii <zheninjas75@yahoo.co.id>
2023-01-18 20:47:54 +0700

    update: add dom to show rgb number

--- random-color/index.js
+++ random-color/index.js
@@ -1,9 +1,14 @@
 const randomNumber = () => Math.floor(Math.random() * 256);
 function changeColor() {
-  let rgbNumber = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+  let rgbNumber = ` rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()}) `;
   document.getElementById("previewColor").style.backgroundColor = rgbNumber;
+  document.getElementById("rgbNumberText").textContent = rgbNumber;
 
   console.log(rgbNumber);
 }
 
-changeColor();
+function runApp() {
+  changeColor();
+}
+
+runApp();
